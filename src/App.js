251a622191f8152ea748e156e4979e78324e7021
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2021-08-27 15:31:58 +0530

    MessageOfLife, Asynchronous Call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState({meaningOfLife:this.state.meaningOfLife+1}, ()=> console.log(this.state.meaningOfLife))
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button onClick={this.handleClick}>update State</button>
+        </header>
+      </div>
+    )
+  }
+}
+
+/*unused class App2 for sample class code*/
+class App2 extends React.Component {
+  render() {
+    return (
+      <div><h1>Test</h1></div>
+    )
+  }
 }
 
 export default App;
