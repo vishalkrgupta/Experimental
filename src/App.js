b7633d626a6ce6207724402d3fc648742fc4d646
VISHAL GUPTA <vishal.kr.gupta@gmail.com>
2021-08-28 23:59:31 +0530

    Handling PrevState and PrevProps

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import './App.css';
 import { render } from '@testing-library/react';
 import React from 'react';
 
+// class App extends React.Component {
+//   state = {
+//       meaningOfLife: 47
+//     }
+  
+//   handleClick = () => {
+//     this.setState((prevState, prevProps) => {
+//       return {
+//         meaningOfLife: prevState.meaningOfLife + 1
+//       }
+//     }, () => console.log(this.state.meaningOfLife))
+//   }
+
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      meaningOfLife: 47
+      meaningOfLife: 47+ this.props.increment
     }
   }
+
+
 
   handleClick = () => {
     this.setState((prevState, prevProps) => {
       return {
-        meaningOfLife: this.state.meaningOfLife + 1
+        meaningOfLife: prevState.meaningOfLife + prevProps.increment
       }
     }, () => console.log(this.state.meaningOfLife))
-
   }
-
   render() {
     return (
       <div className="App">
